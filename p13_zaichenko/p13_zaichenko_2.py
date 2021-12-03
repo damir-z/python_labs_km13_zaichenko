914d580514b802edce8fc544c81fdaad7b372bd5
Damir Zaichenko <zaichenkodamir@gmail.com>
2021-12-04 01:25:52 +0200

    optimized program to make it work faster

--- p13_zaichenko/p13_zaichenko_2.py
+++ p13_zaichenko/p13_zaichenko_2.py
@@ -1,25 +1,15 @@
-import re
-import time
-import fileinput
-#import zipfile
-
-#archive = zipfile.ZipFile('archive.zip', 'r')
 males, females = dict(), dict()
 
-for i in range(1880, 2019):
-    """If you want to work not with a folder, but directly with an archive,
-    uncomment strings 4, 8, 15, and comment string 16 (Getting the results will take longer.)"""
-
-    #with archive.open(f"yob{i}.txt", "r") as f:
+for i in range(1880, 2020):
     with open(f"names/yob{i}.txt", "r") as f:
         male, female = ["", 0], ["", 0]
         for j in f:
-            if re.search(r"[FM](?=,)", str(j))[0] == "M":
-                if int(re.search(r"\d{1,}", str(j))[0]) > male[1]:
-                    male[1], male[0] = int(re.search(r"\d{1,}", str(j))[0]), re.search(r"\w{1,}(?=,[FM])", str(j))[0]
+            if ',M,' in str(j):
+                if int(str(j)[int(j.rfind(","))+1::]) > male[1]:
+                    male[1], male[0] = int(str(j)[int(j.rfind(","))+1::]), str(j)[:int(j.find(",")):]
             else:
-                if int(re.search(r"\d{1,}", str(j))[0]) > female[1]:
-                    female[1], female[0] = int(re.search(r"\d{1,}", str(j))[0]), re.search(r"\w{1,}(?=,[FM])", str(j))[0]
+                if int(str(j)[int(j.rfind(","))+1::]) > female[1]:
+                    female[1], female[0] = int(str(j)[int(j.rfind(","))+1::]), str(j)[:int(j.find(",")):]
 
     if male[0] in males:
         males[male[0]] += 1
